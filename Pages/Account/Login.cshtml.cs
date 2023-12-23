60907353b68d81554b29a834db8a84307f946f8f
Emir Halıcı <emir.halici1@gmail.com>
2023-12-23 19:46:47 +0300

    initial commit

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace git_todo_tracker_web_client.Pages.Account
+{
+    public class Login : PageModel
+    {
+        private readonly ILogger<Login> _logger;
+
+        public Login(ILogger<Login> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
